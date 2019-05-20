125c9722e720ec7fef94fceffb56c97301b58590
Maxim Talanov <minorforyounot@gmail.com>
2019-05-20 11:43:17 +0300

    404 style page

--- client/src/feature/NotFound/container/index.jsx
+++ client/src/feature/NotFound/container/index.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import style from "./index.module.scss";
+import { Link } from "react-router-dom";
 
 export const NotFound = () => {
-  return <div>Not Found Page</div>;
+  return (
+    <div className={style.notFoundWrapper}>
+      <h1>404</h1>
+      <h2>Not Found Page</h2>
+      <p>
+        Go to <Link to="/">Main Page</Link>
+      </p>
+    </div>
+  );
 };
